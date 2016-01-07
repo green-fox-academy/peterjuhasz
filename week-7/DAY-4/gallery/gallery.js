dd24c6be70d0b3bc2b1e85dbcced847a18205990
peterjuhasz <ifj.juhasz.peter@gmail.com>
2016-01-07 23:26:01 +0100

    counter

--- week-7/DAY-4/gallery/gallery.js
+++ week-7/DAY-4/gallery/gallery.js
@@ -17,7 +17,13 @@ var picture = document.querySelector('img');
 var currentIndex = 0;
 var previousButton = document.querySelector('.stepback');
 var nextButton = document.querySelector('.stepforward');
+// var title = document.getElementById('title');
 
+// document.getElementById('title').innerHTML = 'alma';
+
+function refreshTitle(id) {
+    document.getElementById('title').innerHTML = 'Picture ' + id + '.';
+}
 
 function changePicture(src) {
     picture.setAttribute('src', src)
@@ -25,15 +31,32 @@ function changePicture(src) {
 
 previousButton.addEventListener('click',
     function () {
-        currentIndex === 0 ? alert('this is the first picture') : changePicture(kepek[--currentIndex]);
+        currentIndex === 0 ? alert('this is the first picture') : (changePicture(kepek[--currentIndex]), refreshTitle(currentIndex+1));
 })
 
 nextButton.addEventListener('click',
     function () {
-        currentIndex + 1 ===  kepek.length ? alert('this is the last picture') : changePicture(kepek[++currentIndex]);
-
+        currentIndex + 1 ===  kepek.length ? alert('this is the last picture') : (changePicture(kepek[++currentIndex]), refreshTitle(currentIndex+1));
 })
-
+//-------------------Refactoring-------------------------
+// previousButton.addEventListener('click',
+//     function () {
+//         if (currentIndex === 0) {
+//             alert('this is the first picture')
+//         }else {
+//             changePicture(kepek[--currentIndex]);
+//             refreshTitle(currentIndex+1);
+//         }
+// })
+// nextButton.addEventListener('click',
+//     function () {
+//         if (currentIndex + 1 === kepek.length) {
+//             alert('this is the first picture')
+//         }else {
+//             changePicture(kepek[++currentIndex]);
+//             refreshTitle(currentIndex+1);
+//         }
+// })
 
 
 
